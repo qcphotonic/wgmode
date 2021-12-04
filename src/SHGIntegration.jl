7c3f4d57db0a706969be86422e5cc51a7cd2e54f
Liu-Wenzheng <lwz_regis@163.com>
2021-12-05 03:55:40 +0800

    shgintegration

--- src/SHGIntegration.jl
+++ src/SHGIntegration.jl
@@ -1,10 +1,7 @@
 include("ModeSolutions.jl")
 
-d22 = 1
-d31 = 1
-d33 = 1
-
-function Irrr(theta, m_a, m_b)
+function Irrr(theta, m_a, m_b, d)
+    d22, d31, d33 = d
     if 2*m_a-m_b == 0
         return sin(theta)*(d33*cos(theta)^3+3*d31*cos(theta)*sin(theta)^2)
     elseif 2*m_a-m_b == -3
@@ -16,7 +13,8 @@ function Irrr(theta, m_a, m_b)
     end
 end
 
-function Irrt(theta, m_a, m_b)
+function Irrt(theta, m_a, m_b, d)
+    d22, d31, d33 = d
     if 2*m_a-m_b == 0
         return sin(theta)^2*(d31-d33+(3*d31-d33)*cos(2*theta))/2
     elseif 2*m_a-m_b == -3
@@ -28,7 +26,8 @@ function Irrt(theta, m_a, m_b)
     end
 end
 
-function Irrp(theta, m_a, m_b)
+function Irrp(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -(d22+4*d31*cos(theta))*sin(theta)^3/4
     elseif 2*m_a-m_b == -2
@@ -42,7 +41,8 @@ function Irrp(theta, m_a, m_b)
     end
 end
 
-function Irtr(theta, m_a, m_b)
+function Irtr(theta, m_a, m_b, d)
+    d22, d31, d33 = d
     if 2*m_a-m_b == 0
         return sin(theta)^2*(d31-d33+(3*d31-d33)*cos(2*theta))
     elseif 2*m_a-m_b == -3
@@ -54,7 +54,8 @@ function Irtr(theta, m_a, m_b)
     end
 end
 
-function Irtt(theta, m_a, m_b)
+function Irtt(theta, m_a, m_b, d)
+    d22, d31, d33 = d
     if 2*m_a-m_b == 0
         return sin(2*theta)*(d33-d31+(3*d31-d33)*cos(2*theta))/2
     elseif 2*m_a-m_b == -3
@@ -66,7 +67,8 @@ function Irtt(theta, m_a, m_b)
     end
 end
 
-function Irtp(theta, m_a, m_b)
+function Irtp(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -(d22*cos(theta)+2*d31*cos(2*theta))*sin(theta)^2/2
     elseif 2*m_a-m_b == -2
@@ -80,7 +82,8 @@ function Irtp(theta, m_a, m_b)
     end
 end
 
-function Irpr(theta, m_a, m_b)
+function Irpr(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -sin(theta)^3*(d22+4*d31*cos(theta))/2
     elseif 2*m_a-m_b == -2
@@ -94,7 +97,8 @@ function Irpr(theta, m_a, m_b)
     end
 end
 
-function Irpt(theta, m_a, m_b)
+function Irpt(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -sin(theta)^2*(d22*cos(theta)+2*d31*cos(2*theta))/2
     elseif 2*m_a-m_b == -2
@@ -108,7 +112,8 @@ function Irpt(theta, m_a, m_b)
     end
 end
 
-function Irpp(theta, m_a, m_b)
+function Irpp(theta, m_a, m_b, d)
+    d22, d31, = d
     if 2*m_a-m_b == 0
         return sin(theta)*(d31*(3*cos(theta)-cos(3*theta))+4*d22*sin(theta)^2)/2
     elseif 2*m_a-m_b == -1
@@ -126,7 +131,8 @@ function Irpp(theta, m_a, m_b)
     end
 end
 
-function Ittr(theta, m_a, m_b)
+function Ittr(theta, m_a, m_b, d)
+    d22, d31, d33 = d
     if 2*m_a-m_b == 0
         return (d33-d31+(3*d31-d33)*cos(2*theta))*sin(2*theta)/4
     elseif 2*m_a-m_b == -3
@@ -138,7 +144,8 @@ function Ittr(theta, m_a, m_b)
     end
 end
 
-function Ittt(theta, m_a, m_b)
+function Ittt(theta, m_a, m_b, d)
+    d22, d31, d33 = d
     if 2*m_a-m_b == 0
         return -(3*d31+d33+(3*d31-d33)*cos(2*theta))*sin(theta)^2/2
     elseif 2*m_a-m_b == -3
@@ -150,7 +157,8 @@ function Ittt(theta, m_a, m_b)
     end
 end
 
-function Ittp(theta, m_a, m_b)
+function Ittp(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -(d22*cos(theta)-4*d31*sin(theta)^2)*sin(2*theta)/8
     elseif 2*m_a-m_b == -2
@@ -164,7 +172,8 @@ function Ittp(theta, m_a, m_b)
     end
 end
 
-function Itpr(theta, m_a, m_b)
+function Itpr(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -(d22*cos(theta)+2*d31*cos(2*theta))*sin(theta)^2/2
     elseif 2*m_a-m_b == -2
@@ -178,7 +187,8 @@ function Itpr(theta, m_a, m_b)
     end
 end
 
-function Itpt(theta, m_a, m_b)
+function Itpt(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 1
         return -sin(2*theta)*(d22*cos(theta)-4*d31*sin(theta)^2)/4
     elseif 2*m_a-m_b == -2
@@ -192,7 +202,8 @@ function Itpt(theta, m_a, m_b)
     end
 end
 
-function Itpp(theta, m_a, m_b)
+function Itpp(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 0
         return sin(theta)^2*(2*d22*cos(theta)+d31*(cos(2*theta)-2))
     elseif 2*m_a-m_b == -1
@@ -210,7 +221,8 @@ function Itpp(theta, m_a, m_b)
     end
 end
 
-function Ippr(theta, m_a, m_b)
+function Ippr(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 0
         return sin(theta)*(d31*(3*cos(theta)-cos(3*theta))+4*d22*sin(theta)^2)/4
     elseif 2*m_a-m_b == -1
@@ -228,7 +240,8 @@ function Ippr(theta, m_a, m_b)
     end
 end
 
-function Ippt(theta, m_a, m_b)
+function Ippt(theta, m_a, m_b, d)
+    d22, d31, = d
     if abs(2*m_a-m_b) == 0
         return sin(theta)^2*(2*d22*cos(theta)+d31*(cos(2*theta)-2))/2
     elseif 2*m_a-m_b == -1
@@ -246,7 +259,8 @@ function Ippt(theta, m_a, m_b)
     end
 end
 
-function Ippp(theta, m_a, m_b)
+function Ippp(theta, m_a, m_b, d)
+    d22, = d
     if abs(2*m_a-m_b) == 1
         return 3*d22*(sin(3*theta)-2*sin(theta))/8
     elseif abs(2*m_a-m_b) == 3
@@ -255,3 +269,94 @@ function Ippp(theta, m_a, m_b)
         return 0
     end
 end
+
+function theta_depends(theta, m_a, m_b, d, coupling_types)
+    if coupling_types == "rrr"
+        return Irrr(theta, m_a, m_b, d)
+    elseif coupling_types == "rrt"
+        return Irrt(theta, m_a, m_b, d)
+    elseif coupling_types == "rrp"
+        return Irrp(theta, m_a, m_b, d)
+    elseif coupling_types == "rtr"
+        return Irtr(theta, m_a, m_b, d)
+    elseif coupling_types == "rtt"
+        return Irtt(theta, m_a, m_b, d)
+    elseif coupling_types == "rtp"
+        return Irtp(theta, m_a, m_b, d)
+    elseif coupling_types == "rpr"
+        return Irpr(theta, m_a, m_b, d)
+    elseif coupling_types == "rpt"
+        return Irpt(theta, m_a, m_b, d)
+    elseif coupling_types == "rpp"
+        return Irpp(theta, m_a, m_b, d)
+    elseif coupling_types == "ttr"
+        return Ittr(theta, m_a, m_b, d)
+    elseif coupling_types == "ttt"
+        return Ittt(theta, m_a, m_b, d)
+    elseif coupling_types == "ttp"
+        return Ittp(theta, m_a, m_b, d)
+    elseif coupling_types == "tpr"
+        return Itpr(theta, m_a, m_b, d)
+    elseif coupling_types == "tpt"
+        return Itpt(theta, m_a, m_b, d)
+    elseif coupling_types == "tpp"
+        return Itpp(theta, m_a, m_b, d)
+    elseif coupling_types == "ppr"
+        return Ippr(theta, m_a, m_b, d)
+    elseif coupling_types == "ppt"
+        return Ippt(theta, m_a, m_b, d)
+    elseif coupling_types == "ppp"
+        return Ippp(theta, m_a, m_b, d)
+    else
+        println("Error: No $coupling_types found.")
+        return 0
+    end
+end
+
+function overlap_nonlinearity(field_parameters, n, R, d; digit=3)
+    error = 10.0^(-floor(Int, digit/2)-1)
+    rtol = 10.0^(-digit)
+    contribution = complex(zeros(0))
+    region1_r = integral_region(lambda1, l_num1, m_num1, n, R, mode1, "E_r", error=error)
+    region1_theta = integral_region(lambda1, l_num1, m_num1, n, R, mode1, "E_theta", error=error)
+    region1_phi = integral_region(lambda1, l_num1, m_num1, n, R, mode1, "E_phi", error=error)
+    region2_r = integral_region(lambda2, l_num2, m_num2, n, R, mode2, "E_r", error=error)
+    region2_theta = integral_region(lambda2, l_num2, m_num2, n, R, mode2, "E_theta", error=error)
+    region2_phi = integral_region(lambda2, l_num2, m_num2, n, R, mode2, "E_phi", error=error)
+    region = [region1_r, region1_theta, region1_phi, region2_r, region2_theta, region2_phi]
+    println(region)
+    coupling = ["rrr", "rrt", "rrp", "rtr", "rtt", "rtp", "rpr", "rpt", "rpp", "ttr", "ttt", "ttp", "tpr", "tpt", "tpp", "ppr", "ppt", "ppp"]
+    field_type = ["E_r", "E_theta", "E_phi"]
+    for (i, coupling_types) in enumerate(coupling)
+        k1 = floor(Int, floor((i-1)/9)+1+floor((i-1)/15))
+        k2 = floor(Int, floor((i-1)/3)%3+1+floor((i-1)/9)-floor((i-1)/15))
+        k3 = floor(Int, (i-1)%3+4)
+        k4 = floor(Int, (i-1)%3+1)
+        integral = overlap_nonlinearity_calculation(field_parameters, n, R, d, region[k1], region[k2], region[k3], field_type[k1], field_type[k2], field_type[k4], coupling_types, rtol, digit)
+        print("$integral ")
+        append!(contribution, integral)
+    end
+    return sum(contribution)
+end
+
+function overlap_nonlinearity_calculation(field_parameters, n, R, d, region1, region2, region3, field_type1, field_type2, field_type3, coupling_types, rtol, digit)
+    lambda1, l_num1, m_num1, mode1 = field_parameters[1]
+    lambda2, l_num2, m_num2, mode2 = field_parameters[2]
+    if region1[1] !== nothing && region1[2] !== nothing && region2[1] !== nothing && region2[2] !== nothing && region3[1] !== nothing && region3[2] !== nothing
+        mregion = mutual_region(region3, mutual_region(region1, region2))
+        region = [mregion[1], mregion[2], R, pi/2]
+        # print(region)
+        f1(p) = field(p[1], p[2], lambda1, l_num1, m_num1, n, R, mode1, field_type1)
+        f2(p) = field(p[1], p[2], lambda1, l_num1, m_num1, n, R, mode1, field_type2)
+        f3(p) = conj(field(p[1], p[2], lambda2, l_num2, m_num2, n, R, mode2, field_type3))
+        f4(p) = p[1]^2*f1(p)*f2(p)*f3(p)*theta_depends(p[2], m_num1, m_num2, d, coupling_types)
+        integration = abs(2*m_num1-m_num2) <= 3 ? hcubature(f4, region[1:2], region[3:4], rtol=rtol)[1] : 0
+        return round(integration, sigdigits=digit)
+    else
+        return 0
+    end
+end
+    
+
+    
+
